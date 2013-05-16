e62dae2940d2f4031aa0a3b2e40a93660544c1ac
Christopher Bertels <bakkdoor@flasht.de>
2013-05-16 22:48:21 +0200

    update example bot

--- examples/hello_bot.d
+++ examples/hello_bot.d
@@ -1,20 +1,28 @@
 module main;
 
 import std.stdio;
-import ircbod.client;
+import ircbod.client, ircbod.message;
 
 void main(string[] args)
 {
     IRCClient bot = new IRCClient("irc.freenode.net", 6667, "ircbod", null, ["#ircbod"]);
 
-    bot.on(MessageType.MESSAGE, r"^!hello (\S+)$", (msg, args) {
-        msg.reply("Hello to you, too, " ~ msg.nickname);
+    bot.on(IRCMessage.Type.PRIV_MESSAGE, r"^hello (\S+)$", (msg, args) {
+        msg.reply("Hello to you, too " ~ msg.nickname ~ "! You greeted: " ~ args[0]);
     });
 
-    bot.on(MessageType.JOIN, (msg) {
+    bot.on(IRCMessage.Type.JOIN, (msg) {
         writeln("User joined: ", msg.nickname);
         if(msg.nickname != bot.name)
             msg.reply("Welcome to the channel, " ~ msg.nickname);
+    });
+
+    bot.on(IRCMessage.Type.CHAN_MESSAGE, (msg) {
+        writeln("got chan message: " ~ msg.text);
+    });
+
+    bot.on(IRCMessage.Type.PRIV_MESSAGE, (msg) {
+        writeln("got private message: " ~ msg.text);
     });
 
     bot.run();
